200d519f9a01c0ff474c7add4dc817261df20bb6
Oleh <olegbudurovych@gmail.com>
2018-09-03 18:36:32 +0300

    update server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express'),
-  app = express();
+    app = express();
+
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
-app.get('/', (req, res) => res.render('src/index'))
+
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
